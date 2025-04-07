54a0d509a6efefb4c120132edf2410c5d4a8bdc3
fermartz <fernandomvc@gmail.com>
2025-04-07 10:54:09 -0500

    remove console.logs

--- src/siwb-provider.tsx
+++ src/siwb-provider.tsx
@@ -62,11 +62,11 @@ export async function callLogin(
   if (!anonymousActor || !data || !address || !publickeyHex || !signMessageType) {
     throw new Error('Invalid actor, data , address or signMessageType');
   }
-console.log("address", address);
-console.log("publickeyHex", publickeyHex);
-console.log("sessionPublicKey", sessionPublicKey);
-console.log("signMessageType", signMessageType);
-console.log("data", data);
+// console.log("address", address);
+// console.log("publickeyHex", publickeyHex);
+// console.log("sessionPublicKey", sessionPublicKey);
+// console.log("signMessageType", signMessageType);
+// console.log("data", data);
 
 const loginReponse = await anonymousActor.siwb_login(
   data,
@@ -75,7 +75,7 @@ const loginReponse = await anonymousActor.siwb_login(
   new Uint8Array(sessionPublicKey),
   signMessageType
 );
-console.log(loginReponse);
+// console.log(loginReponse);
   if ('Err' in loginReponse) {
     throw new Error(loginReponse.Err);
   }
